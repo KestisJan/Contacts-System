267ed0f6bfdd92e4c123adda49d63de8db088b32
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-02 17:32:24 +0300

    Add NotFoundView component and update router configuration
    - Created  to handle 404 errors with a user-friendly message.
    - Modified  to include a route for the NotFoundView.

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+            <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">404 Puslapis nerastas</h1>
+            <p class="mt-6 text-base leading-7 text-gray-600">Atsiprašome, neradome puslapio, kurio ieškote.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+                <router-link to="/" class="bg-[#0054A6] rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2">Grįžti į pradžią</router-link>
+            </div>
+        </div>
+    </main>
+  </template>
+  
