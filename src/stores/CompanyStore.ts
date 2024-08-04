bde94cbd4de75194d2337a070c2be4adc60e43db
KestisJan <131695435+KestisJan@users.noreply.github.com>
2024-08-04 12:17:24 +0300

    Refactor CompanyStore.ts to utilize genericService.ts
    - Replaced direct service calls with reusable methods from genericService.ts.
    - Implemented generic methods for fetching company data by ID and with filters.
    - Added methods 'getCompanyById' and 'getCompanyByFilter' to streamline data retrieval.
    - Updated store to use generic types, enhancing reusability and maintainability.

--- src/stores/CompanyStore.ts
+++ src/stores/CompanyStore.ts
@@ -1,38 +1,57 @@
 import { defineStore } from "pinia";
-import companyService from "../service/companyService";
+import genericService from "../service/genericService";
 import { readonly, ref } from 'vue'
 import { type ICompany, ICompanyListResponse } from "../interface/ICompany";
 
 export const useCompanyStore = defineStore("companyStore", () => {
   const companies = ref<ICompany[]>([]);  
   const totalCount = ref<number>(0);
-
+  const selectedCompany = ref<ICompany | null>(null);
 
   const fetchCompanies = async () => {
     try {
-        const response: ICompanyListResponse = await companyService.getFullList()
-        companies.value = response.items;
-        totalCount.value = response.totalCount;
+      const response = await genericService.getList<ICompany>('companies');
+      companies.value = response.items;
+      totalCount.value = response.totalCount;
     } catch (err: any) {
-        console.error('Failed to fetch companies', err)
-    } 
+      console.error('Failed to fetch companies', err)
+    }
   };
 
   const loadAllCompanies = async () => {
     try {
-      const allCompanies = await companyService.getAllCompanies();
-      companies.value = allCompanies;
+      const allCompanies = await genericService.getAll<ICompany>('companies');
+      companies.value = allCompanies.items;
+      totalCount.value = allCompanies.totalCount;
     } catch (err: any) {
-      console.error('Failed to load all companies:', err);
+      console.error('Failed to load all companies: ', err);
     }
-
   };
 
+  const getCompanyById = async (id: string) => {
+    try {
+      selectedCompany.value = await genericService.getById<ICompany>('companies', id);
+    } catch (err: any) {
+      console.error(`Failed to fetch company with ID ${id}:`, err);
+    }
+  };
+
+
+  const getCompanyByFilter = async (filter: string) => {
+    try {
+      selectedCompany.value = await genericService.getFirstListItem<ICompany>('companies', filter);
+    } catch (err: any) {
+      console.error(`Failed to fetch company with filter ${filter}:`, err);
+    }
+  };
 
   return {
     companies:readonly(companies),
     totalCount,
     fetchCompanies,
     loadAllCompanies,
+    getCompanyById,
+    getCompanyByFilter
+    
   };
 });
